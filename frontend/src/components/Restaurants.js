a7e3cfddd625ea74632f5b582663ff3d0d4d411f
Amal Khan <amalfkhan@gmail.com>
2021-05-17 12:54:06 -0700

    added displaying of reviews for a restaurant

--- frontend/src/components/Restaurants.js
+++ frontend/src/components/Restaurants.js
@@ -12,8 +12,6 @@ const Restaurant = (props) => {
     reviews: []
   });
   
-  console.log(restaurant);
-
   useEffect(() => {
     getRestaurant(props.match.params.id);
   }, [props.match.params.id]); //only call if this is updated
@@ -44,6 +42,52 @@ const Restaurant = (props) => {
 
   return (
     <div>
+      <div>
+        {restaurant 
+        ? (
+          <div>
+            <h5>{restaurant.name}</h5>
+            <p>
+              <strong>Cuisine: </strong>{restaurant.cuisine}<br/>
+              <strong>Address: </strong>{restaurant.address.building} {restaurant.address.street}, {restaurant.address.zipcode}
+            </p>
+            <h4> Reviews </h4>
+            <div className="row">
+              {restaurant.reviews.length > 0 
+              ? (
+                restaurant.reviews.map((review, index) => {
+                  return (
+                    <div className="col-lg-4 pb-1" key={index}>
+                      <div className="card">
+                        <div className="card-body">
+                          <p className="card-text">
+                            {review.text}<br/>
+                            <strong>User: </strong>{review.name}<br/>
+                            <strong>Date: </strong>{review.date}
+                          </p>
+                        </div>
+                      </div>
+                    </div>
+                  );
+                })
+              ) 
+              : (
+                <div className="col-sm-4">
+                  <p>No reviews yet.</p>
+                </div>
+              )}
+
+            </div>
+
+          </div>
+        ) 
+        : (
+          <div>
+            <br />
+            <p>No restaurant selected.</p>
+          </div>
+        )}
+      </div>
     </div>
   );
 }

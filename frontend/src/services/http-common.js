fd1d20e890d5726bdae6ab22fd3e68d9b6a9aa41
Amal Khan <amalfkhan@gmail.com>
2021-06-03 19:28:57 -0700

    updated baseurl

--- frontend/src/services/http-common.js
+++ frontend/src/services/http-common.js
@@ -5,7 +5,8 @@ import axios from "axios";
 export default axios.create({
   withCredentials: true,
   credentials: "include",
-  baseURL: "http://localhost:5000/api/v1/restaurants",
+  // baseURL: process.env.baseURL || "http://localhost:5000/api/v1/restaurants",
+  baseURL: "/api/v1/restaurants",
   headers: {
     "Content-type": "application/json",
   },

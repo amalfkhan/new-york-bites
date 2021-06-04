91a26c77c877bae513caf3a8bc2d508a17e71728
Amal Khan <amalfkhan@gmail.com>
2021-06-03 17:56:32 -0700

    reorginized folder structure for deployment

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 // setup base access point for routes in the api
 import express from "express";
 import cors from "cors";
+import path from "path";
 import cookieParser from "cookie-parser";
 import routes from "./api/routes.js";
 const app = express();
@@ -12,5 +13,12 @@ app.use(cookieParser());
 
 app.use("/api/v1/restaurants", routes);
 app.use("*", (req, res) => res.status(404).json({ error: "not found" }));
+
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static("../frontend/build"));
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.resolve(__dirname, "../frontend", "build", "index.html"));
+//   });
+// }
 
 export default app;
